addbf16c0b4897d01fcfaab4de9a8751ab2604db
Greg Pfaff <greg.pfaff@razorfish.com>
2016-03-10 14:12:35 -0600

    adding test

--- index.js
+++ index.js
@@ -22,3 +22,819 @@ ig.use({
 app.listen(2000);
 
 console.log("Server has started");
+
+/*
+var express = require('express');
+var app = express();
+var ig = require('instagram-node').instagram();
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
+
+app.get('/', function(req, res){
+		res.render('pages/index');
+});
+
+ig.use({
+	client_id: '9873603e937d45968de0a4db5831eccb',
+	client_secret:	'12bc68f7b9584b5fbacf5306b56cc6e5'
+});
+
+app.listen(2000);
+
+console.log("Server has started");
+
+var express = require('express');
+var app = express();
+var ig = require('instagram-node').instagram();
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
+
+app.get('/', function(req, res){
+		res.render('pages/index');
+});
+
+ig.use({
+	client_id: '9873603e937d45968de0a4db5831eccb',
+	client_secret:	'12bc68f7b9584b5fbacf5306b56cc6e5'
+});
+
+app.listen(2000);
+
+console.log("Server has started");
+
+var express = require('express');
+var app = express();
+var ig = require('instagram-node').instagram();
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
+
+app.get('/', function(req, res){
+		res.render('pages/index');
+});
+
+ig.use({
+	client_id: '9873603e937d45968de0a4db5831eccb',
+	client_secret:	'12bc68f7b9584b5fbacf5306b56cc6e5'
+});
+
+app.listen(2000);
+
+console.log("Server has started");
+
+var express = require('express');
+var app = express();
+var ig = require('instagram-node').instagram();
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
+
+app.get('/', function(req, res){
+		res.render('pages/index');
+});
+
+ig.use({
+	client_id: '9873603e937d45968de0a4db5831eccb',
+	client_secret:	'12bc68f7b9584b5fbacf5306b56cc6e5'
+});
+
+app.listen(2000);
+
+console.log("Server has started");
+
+var express = require('express');
+var app = express();
+var ig = require('instagram-node').instagram();
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
+
+app.get('/', function(req, res){
+		res.render('pages/index');
+});
+
+ig.use({
+	client_id: '9873603e937d45968de0a4db5831eccb',
+	client_secret:	'12bc68f7b9584b5fbacf5306b56cc6e5'
+});
+
+app.listen(2000);
+
+console.log("Server has started");
+
+var express = require('express');
+var app = express();
+var ig = require('instagram-node').instagram();
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
+
+app.get('/', function(req, res){
+		res.render('pages/index');
+});
+
+ig.use({
+	client_id: '9873603e937d45968de0a4db5831eccb',
+	client_secret:	'12bc68f7b9584b5fbacf5306b56cc6e5'
+});
+
+app.listen(2000);
+
+console.log("Server has started");
+
+var express = require('express');
+var app = express();
+var ig = require('instagram-node').instagram();
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
+
+app.get('/', function(req, res){
+		res.render('pages/index');
+});
+
+ig.use({
+	client_id: '9873603e937d45968de0a4db5831eccb',
+	client_secret:	'12bc68f7b9584b5fbacf5306b56cc6e5'
+});
+
+app.listen(2000);
+
+console.log("Server has started");
+
+var express = require('express');
+var app = express();
+var ig = require('instagram-node').instagram();
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
+
+app.get('/', function(req, res){
+		res.render('pages/index');
+});
+
+ig.use({
+	client_id: '9873603e937d45968de0a4db5831eccb',
+	client_secret:	'12bc68f7b9584b5fbacf5306b56cc6e5'
+});
+
+app.listen(2000);
+
+console.log("Server has started");
+
+var express = require('express');
+var app = express();
+var ig = require('instagram-node').instagram();
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
+
+app.get('/', function(req, res){
+		res.render('pages/index');
+});
+
+ig.use({
+	client_id: '9873603e937d45968de0a4db5831eccb',
+	client_secret:	'12bc68f7b9584b5fbacf5306b56cc6e5'
+});
+
+app.listen(2000);
+
+console.log("Server has started");
+
+var express = require('express');
+var app = express();
+var ig = require('instagram-node').instagram();
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
+
+app.get('/', function(req, res){
+		res.render('pages/index');
+});
+
+ig.use({
+	client_id: '9873603e937d45968de0a4db5831eccb',
+	client_secret:	'12bc68f7b9584b5fbacf5306b56cc6e5'
+});
+
+app.listen(2000);
+
+console.log("Server has started");
+
+var express = require('express');
+var app = express();
+var ig = require('instagram-node').instagram();
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
+
+app.get('/', function(req, res){
+		res.render('pages/index');
+});
+
+ig.use({
+	client_id: '9873603e937d45968de0a4db5831eccb',
+	client_secret:	'12bc68f7b9584b5fbacf5306b56cc6e5'
+});
+
+app.listen(2000);
+
+console.log("Server has started");
+
+var express = require('express');
+var app = express();
+var ig = require('instagram-node').instagram();
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
+
+app.get('/', function(req, res){
+		res.render('pages/index');
+});
+
+ig.use({
+	client_id: '9873603e937d45968de0a4db5831eccb',
+	client_secret:	'12bc68f7b9584b5fbacf5306b56cc6e5'
+});
+
+app.listen(2000);
+
+console.log("Server has started");
+
+var express = require('express');
+var app = express();
+var ig = require('instagram-node').instagram();
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
+
+app.get('/', function(req, res){
+		res.render('pages/index');
+});
+
+ig.use({
+	client_id: '9873603e937d45968de0a4db5831eccb',
+	client_secret:	'12bc68f7b9584b5fbacf5306b56cc6e5'
+});
+
+app.listen(2000);
+
+console.log("Server has started");
+
+var express = require('express');
+var app = express();
+var ig = require('instagram-node').instagram();
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
+
+app.get('/', function(req, res){
+		res.render('pages/index');
+});
+
+ig.use({
+	client_id: '9873603e937d45968de0a4db5831eccb',
+	client_secret:	'12bc68f7b9584b5fbacf5306b56cc6e5'
+});
+
+app.listen(2000);
+
+console.log("Server has started");
+
+var express = require('express');
+var app = express();
+var ig = require('instagram-node').instagram();
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
+
+app.get('/', function(req, res){
+		res.render('pages/index');
+});
+
+ig.use({
+	client_id: '9873603e937d45968de0a4db5831eccb',
+	client_secret:	'12bc68f7b9584b5fbacf5306b56cc6e5'
+});
+
+app.listen(2000);
+
+console.log("Server has started");
+
+var express = require('express');
+var app = express();
+var ig = require('instagram-node').instagram();
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
+
+app.get('/', function(req, res){
+		res.render('pages/index');
+});
+
+ig.use({
+	client_id: '9873603e937d45968de0a4db5831eccb',
+	client_secret:	'12bc68f7b9584b5fbacf5306b56cc6e5'
+});
+
+app.listen(2000);
+
+console.log("Server has started");
+
+var express = require('express');
+var app = express();
+var ig = require('instagram-node').instagram();
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
+
+app.get('/', function(req, res){
+		res.render('pages/index');
+});
+
+ig.use({
+	client_id: '9873603e937d45968de0a4db5831eccb',
+	client_secret:	'12bc68f7b9584b5fbacf5306b56cc6e5'
+});
+
+app.listen(2000);
+
+console.log("Server has started");
+
+var express = require('express');
+var app = express();
+var ig = require('instagram-node').instagram();
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
+
+app.get('/', function(req, res){
+		res.render('pages/index');
+});
+
+ig.use({
+	client_id: '9873603e937d45968de0a4db5831eccb',
+	client_secret:	'12bc68f7b9584b5fbacf5306b56cc6e5'
+});
+
+app.listen(2000);
+
+console.log("Server has started");
+
+var express = require('express');
+var app = express();
+var ig = require('instagram-node').instagram();
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
+
+app.get('/', function(req, res){
+		res.render('pages/index');
+});
+
+ig.use({
+	client_id: '9873603e937d45968de0a4db5831eccb',
+	client_secret:	'12bc68f7b9584b5fbacf5306b56cc6e5'
+});
+
+app.listen(2000);
+
+console.log("Server has started");
+
+var express = require('express');
+var app = express();
+var ig = require('instagram-node').instagram();
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
+
+app.get('/', function(req, res){
+		res.render('pages/index');
+});
+
+ig.use({
+	client_id: '9873603e937d45968de0a4db5831eccb',
+	client_secret:	'12bc68f7b9584b5fbacf5306b56cc6e5'
+});
+
+app.listen(2000);
+
+console.log("Server has started");
+
+var express = require('express');
+var app = express();
+var ig = require('instagram-node').instagram();
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
+
+app.get('/', function(req, res){
+		res.render('pages/index');
+});
+
+ig.use({
+	client_id: '9873603e937d45968de0a4db5831eccb',
+	client_secret:	'12bc68f7b9584b5fbacf5306b56cc6e5'
+});
+
+app.listen(2000);
+
+console.log("Server has started");
+
+var express = require('express');
+var app = express();
+var ig = require('instagram-node').instagram();
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
+
+app.get('/', function(req, res){
+		res.render('pages/index');
+});
+
+ig.use({
+	client_id: '9873603e937d45968de0a4db5831eccb',
+	client_secret:	'12bc68f7b9584b5fbacf5306b56cc6e5'
+});
+
+app.listen(2000);
+
+console.log("Server has started");
+
+var express = require('express');
+var app = express();
+var ig = require('instagram-node').instagram();
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
+
+app.get('/', function(req, res){
+		res.render('pages/index');
+});
+
+ig.use({
+	client_id: '9873603e937d45968de0a4db5831eccb',
+	client_secret:	'12bc68f7b9584b5fbacf5306b56cc6e5'
+});
+
+app.listen(2000);
+
+console.log("Server has started");
+
+var express = require('express');
+var app = express();
+var ig = require('instagram-node').instagram();
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
+
+app.get('/', function(req, res){
+		res.render('pages/index');
+});
+
+ig.use({
+	client_id: '9873603e937d45968de0a4db5831eccb',
+	client_secret:	'12bc68f7b9584b5fbacf5306b56cc6e5'
+});
+
+app.listen(2000);
+
+console.log("Server has started");
+
+var express = require('express');
+var app = express();
+var ig = require('instagram-node').instagram();
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
+
+app.get('/', function(req, res){
+		res.render('pages/index');
+});
+
+ig.use({
+	client_id: '9873603e937d45968de0a4db5831eccb',
+	client_secret:	'12bc68f7b9584b5fbacf5306b56cc6e5'
+});
+
+app.listen(2000);
+
+console.log("Server has started");
+
+var express = require('express');
+var app = express();
+var ig = require('instagram-node').instagram();
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
+
+app.get('/', function(req, res){
+		res.render('pages/index');
+});
+
+ig.use({
+	client_id: '9873603e937d45968de0a4db5831eccb',
+	client_secret:	'12bc68f7b9584b5fbacf5306b56cc6e5'
+});
+
+app.listen(2000);
+
+console.log("Server has started");
+
+var express = require('express');
+var app = express();
+var ig = require('instagram-node').instagram();
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
+
+app.get('/', function(req, res){
+		res.render('pages/index');
+});
+
+ig.use({
+	client_id: '9873603e937d45968de0a4db5831eccb',
+	client_secret:	'12bc68f7b9584b5fbacf5306b56cc6e5'
+});
+
+app.listen(2000);
+
+console.log("Server has started");
+
+var express = require('express');
+var app = express();
+var ig = require('instagram-node').instagram();
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
+
+app.get('/', function(req, res){
+		res.render('pages/index');
+});
+
+ig.use({
+	client_id: '9873603e937d45968de0a4db5831eccb',
+	client_secret:	'12bc68f7b9584b5fbacf5306b56cc6e5'
+});
+
+app.listen(2000);
+
+console.log("Server has started");
+
+var express = require('express');
+var app = express();
+var ig = require('instagram-node').instagram();
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
+
+app.get('/', function(req, res){
+		res.render('pages/index');
+});
+
+ig.use({
+	client_id: '9873603e937d45968de0a4db5831eccb',
+	client_secret:	'12bc68f7b9584b5fbacf5306b56cc6e5'
+});
+
+app.listen(2000);
+
+console.log("Server has started");
+
+var express = require('express');
+var app = express();
+var ig = require('instagram-node').instagram();
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
+
+app.get('/', function(req, res){
+		res.render('pages/index');
+});
+
+ig.use({
+	client_id: '9873603e937d45968de0a4db5831eccb',
+	client_secret:	'12bc68f7b9584b5fbacf5306b56cc6e5'
+});
+
+app.listen(2000);
+
+console.log("Server has started");
+
+var express = require('express');
+var app = express();
+var ig = require('instagram-node').instagram();
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
+
+app.get('/', function(req, res){
+		res.render('pages/index');
+});
+
+ig.use({
+	client_id: '9873603e937d45968de0a4db5831eccb',
+	client_secret:	'12bc68f7b9584b5fbacf5306b56cc6e5'
+});
+
+app.listen(2000);
+
+console.log("Server has started");
+
+var express = require('express');
+var app = express();
+var ig = require('instagram-node').instagram();
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
+
+app.get('/', function(req, res){
+		res.render('pages/index');
+});
+
+ig.use({
+	client_id: '9873603e937d45968de0a4db5831eccb',
+	client_secret:	'12bc68f7b9584b5fbacf5306b56cc6e5'
+});
+
+app.listen(2000);
+
+console.log("Server has started");
+
+var express = require('express');
+var app = express();
+var ig = require('instagram-node').instagram();
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
+
+app.get('/', function(req, res){
+		res.render('pages/index');
+});
+
+ig.use({
+	client_id: '9873603e937d45968de0a4db5831eccb',
+	client_secret:	'12bc68f7b9584b5fbacf5306b56cc6e5'
+});
+
+app.listen(2000);
+
+console.log("Server has started");
+
+var express = require('express');
+var app = express();
+var ig = require('instagram-node').instagram();
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
+
+app.get('/', function(req, res){
+		res.render('pages/index');
+});
+
+ig.use({
+	client_id: '9873603e937d45968de0a4db5831eccb',
+	client_secret:	'12bc68f7b9584b5fbacf5306b56cc6e5'
+});
+
+app.listen(2000);
+
+console.log("Server has started");
+
+var express = require('express');
+var app = express();
+var ig = require('instagram-node').instagram();
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
+
+app.get('/', function(req, res){
+		res.render('pages/index');
+});
+
+ig.use({
+	client_id: '9873603e937d45968de0a4db5831eccb',
+	client_secret:	'12bc68f7b9584b5fbacf5306b56cc6e5'
+});
+
+app.listen(2000);
+
+console.log("Server has started");
+
+var express = require('express');
+var app = express();
+var ig = require('instagram-node').instagram();
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
+
+app.get('/', function(req, res){
+		res.render('pages/index');
+});
+
+ig.use({
+	client_id: '9873603e937d45968de0a4db5831eccb',
+	client_secret:	'12bc68f7b9584b5fbacf5306b56cc6e5'
+});
+
+app.listen(2000);
+
+console.log("Server has started");
+
+var express = require('express');
+var app = express();
+var ig = require('instagram-node').instagram();
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
+
+app.get('/', function(req, res){
+		res.render('pages/index');
+});
+
+ig.use({
+	client_id: '9873603e937d45968de0a4db5831eccb',
+	client_secret:	'12bc68f7b9584b5fbacf5306b56cc6e5'
+});
+
+app.listen(2000);
+
+console.log("Server has started");
+*/
